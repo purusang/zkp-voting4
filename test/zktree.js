a98f94527e5fe7829993a984acbb7682ec181291
Purushotam <purushotamsangroula@gmail.com>
2023-07-09 10:13:43 +0545

    Successfully voted twice

--- test/zktree.js
+++ test/zktree.js
@@ -65,8 +65,8 @@ describe("ZKTree Smart contract test", () => {
         // // register 3 voters
         const commitment1 = await generateCommitment()
         await zktreevote.connect(signer).registerCommitment(1, commitment1.commitment)
-        // const commitment2 = await generateCommitment()
-        // await zktreevote.connect(signers[1]).registerCommitment(2, commitment2.commitment)
+        const commitment2 = await generateCommitment()
+        await zktreevote.connect(signer).registerCommitment(2, commitment2.commitment)
         // const commitment3 = await generateCommitment()
         // await zktreevote.connect(signers[1]).registerCommitment(3, commitment3.commitment)
         // // console.log("commitments: ", commitment1, commitment2, commitment3);
@@ -74,13 +74,12 @@ describe("ZKTree Smart contract test", () => {
         // // votes
         // // console.log(signers);
         const cd1 = await calculateMerkleRootAndZKProof(zktreevote.address, signer, TREE_LEVELS, commitment1, "keys/Verifier.zkey")
-        console.log("CD1: ", cd1);
         await zktreevote.connect(signer).vote(1, cd1.nullifierHash, cd1.root, cd1.proof_a, cd1.proof_b, cd1.proof_c)
         console.log("Yes I voted for the first candidate.");
-        // await zktreevote.connect(signer).vote(1, cd1.nullifierHash, cd1.root, cd1.proof_a, cd1.proof_b, cd1.proof_c)
-        // console.log("I voted for the first candidate twice.");
-        // const cd2 = await calculateMerkleRootAndZKProof(zktreevote.target, signers[3], TREE_LEVELS, commitment2, "keys/Verifier.zkey")
-        // await zktreevote.connect(signers[3]).vote(1, cd2.nullifierHash, cd2.root, cd2.proof_a, cd2.proof_b, cd2.proof_c)
+
+        const cd2 = await calculateMerkleRootAndZKProof(zktreevote.address, signer, TREE_LEVELS, commitment2, "keys/Verifier.zkey")
+        await zktreevote.connect(signer).vote(1, cd2.nullifierHash, cd2.root, cd2.proof_a, cd2.proof_b, cd2.proof_c)
+        console.log("I voted for the first candidate twice.");
         // const cd3 = await calculateMerkleRootAndZKProof(zktreevote.target, signers[4], TREE_LEVELS, commitment3, "keys/Verifier.zkey")
         // await zktreevote.connect(signers[4]).vote(2, cd3.nullifierHash, cd3.root, cd3.proof_a, cd3.proof_b, cd3.proof_c)
 
