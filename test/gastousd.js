34c284020a00e624cbdb2dd8a3aab76482e54780
puru <purushotamsangroula@gmail.com>
2023-12-09 23:37:51 +0545

    gas cost calculation

--- test/gastousd.js
+++ test/gastousd.js
@@ -17,8 +17,23 @@ describe("ZKTree Smart contract test", () => {
 
 
     it("Test gas to used", async () => {
-        let usd = convertGasToUsd(61270.667);
-        console.log("convert GasToUsd ", usd);
+        let dep_cost = 2582302 + 1483361 + 3281960;
+        let cand = 161270;
+        let voter = 1064248;
+        let ballot = 573392;
+
+        let admin_cost = ((dep_cost )) //+ voter*40 // + ballot
+        let voter_cost = ballot + voter // + ballot
+        // let tot_cost =   ballot
+        let usd = convertGasToUsd(admin_cost);
+        console.log(`Admin Gas cost ${admin_cost} GasToUsd `, usd);
+
+        usd = convertGasToUsd(voter_cost);
+        console.log(`Voter Gas cost ${voter_cost} GasToUsd `, usd);
+
+        total_cost_per_voter = ((dep_cost )/500) + voter + ballot
+        usd = convertGasToUsd(total_cost_per_voter);
+        console.log(`Total Gas cost/voter ${total_cost_per_voter} GasToUsd `, usd);
     });
     
 });
